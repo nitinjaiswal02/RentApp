52072af73c7cd4938199ee0243e05ac5f83b8793
nitinjaiswal02 <nitinjaiswal@gmail.com>
2025-09-16 22:32:29 +0530

    frontend + backent 5

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
 }));
-app.options("*", cors()); // allow all preflight requests
+app.options("/*", cors()); // allow all preflight requests
 
 // ✅ Body parser (parse JSON request bodies)
 app.use(bodyParser.json());
